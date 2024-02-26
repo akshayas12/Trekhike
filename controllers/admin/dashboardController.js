07b8496980f7b65970e82823c7b41ba36c6332a2
Akshaya Suresh <akshayas517@gmail.com>
2024-02-26 21:36:29 +0530

    controllers/admin/dashboardController.js

--- controllers/admin/dashboardController.js
+++ controllers/admin/dashboardController.js
@@ -7,8 +7,9 @@ const { order } = require('./orderController');
 
 async function generatePDF() {
     // Launch a new browser instance
-    const browser = await puppeteer.launch();
-
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
     // Create a new page
     const page = await browser.newPage();
 
