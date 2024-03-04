82372fbe3710ba01e0838bf751062b9df7ebefaf
Akshaya Suresh <akshayas517@gmail.com>
2024-03-04 16:15:47 +0530

    controllers/admin/dashboardController.js

--- controllers/admin/dashboardController.js
+++ controllers/admin/dashboardController.js
@@ -1,129 +1,122 @@
 const User = require('../../models/userModel');
 const Order = require('../../models/orderModel');
 const Product=require('../../models/productModel')
+const pdfs = require('pdfkit');
+const table = require('pdfkit-table');
+const ejs = require('ejs');
+const fs = require('fs');
+const util = require('util');
+const pdf = require('html-pdf');
+const ExcelJS = require('exceljs');
 
 const puppeteer = require('puppeteer');
 const { order } = require('./orderController');
-
-// exports.getUserDetailsAndOrders = async (req, res) => {
-//   try {
-//     // Aggregate user logins by month with month names
-//     const monthlyLogins = await User.aggregate([
-//       {
-//         $group: {
-//           _id: { $month: "$createdAt" },
-//           monthName: { $first: { $dateToString: { format: "%B", date: "$createdAt" } } },
-//           count: { $sum: 1 }
-//         }
-//       },
-//       { $sort: { "_id": 1 } }
-//     ]);
-
-//     // Generate data for all months, filling in missing months with count 0
-//     const allMonths = Array.from({ length: 12 }, (_, index) => {
-//       const month = index + 1;
-//       const monthName = new Date(2000, month - 1, 1).toLocaleString('en-US', { month: 'long' });
-//       return { _id: month, monthName, count: 0 };
-//     });
-
-//     // Merge the aggregated data with the generated data to ensure all months are represented
-//     const mergedData = allMonths.map(month => {
-//       const match = monthlyLogins.find(entry => entry._id === month._id);
-//       return match || month;
-//     });
-
-//     // Fetch most selling product details
-//     const mostSellingProduct = await Order.aggregate([
-//       { $unwind: "$products" },
-//       {
-//         $group: {
-//           _id: "$products.product",
-//           totalQuantitySold: { $sum: "$products.quantity" }
-//         }
-//       },
-//       { $sort: { totalQuantitySold: -1 } },
-//       { $limit: 1 }
-//     ]);
-
-//     const mostSellingProductDetails = await Product.findById(mostSellingProduct[0]._id);
-
-//     // Fetch delivered orders
-//     const deliveredOrders = await Order.find({ status: 'delivered' })
-//     .populate('userId', 'name') // Populate user details
-//     .populate('products.product', 'productName quantity price'); // Populate product details
-
-//     // Count total pending orders
-//     const pendingOrdersCount = await Order.countDocuments({ status: 'Pending' });
-//     const returnOrderCount = await Order.countDocuments({ status: 'Returned' });
-//     const blockUser = await User.countDocuments({ isBlocked: 'true' });
-//     const onlinePayment = await Order.countDocuments({ payment: 'onlinePayment' });
-
-//     // Count total users, total orders, total cancelled orders, total products, and total revenue
-//     const totalUsers = await User.countDocuments();
-//     const totalOrders = await Order.countDocuments();
-//     const cancelledOrders = await Order.countDocuments({ status: 'Cancelled' });
-//     const totalProduct = await Product.countDocuments();
-//     const totalRevenue = await getTotalRevenue();
-
-//     // Render the sales report template with the fetched data
-//     res.render('sales-report', {
-//       totalUsers,
-//       totalOrders,
-//       cancelledOrders,
-//       blockUser,
-//       totalProduct,
-//       totalRevenue,
-//       mergedData,
-//       deliveredOrders,
-//       mostSellingProduct: mostSellingProductDetails,
-//       returnOrderCount,
-//       onlinePayment,
-//       pendingOrdersCount,
-    
-//   }, function(err, html) {
-//     if (err) {
-//       console.error('Error rendering EJS template:', err.message);
-//       return res.status(500).send('Internal Server Error');
-//     }
-
-//     // Convert the rendered HTML to a PDF
-//     pdf.create(html).toBuffer(function(err, buffer) {
-//       if (err) {
-//         console.error('Error generating PDF:', err.message);
-//         return res.status(500).send('An error occurred while generating the PDF.');
-//       }
-
-//       // Send the PDF as a response
-//       res.set({ 'Content-Type': 'application/pdf', 'Content-Length': buffer.length });
-//       res.send(buffer);
-//     });
-//   });
-// } catch (error) {
-//   console.error('Error fetching user details and orders:', error.message);
-//   res.status(500).send('Internal Server Error');
-// }
-// };
+const { getUserDetailsAndOrders } =require('../../controllers/admin/adminController')
 
 
-async function generatePDF() {
-    // Launch a new browser instance
-    const browser = await puppeteer.launch();
+const renderFileAsync = util.promisify(ejs.renderFile);
 
-    // Create a new page
-    const page = await browser.newPage();
+exports.downloadSalesReports = async (req, res) => {
+  try {
+    // Get other necessary data
+    const { totalUsers, blockUser, mergedData, totalOrders, cancelledOrders, totalProduct, totalRevenue, deliveredOrders, mostSellingProduct, returnOrderCount, onlinePayment, pendingOrdersCount } = await getUserDetailsAndOrders();
 
-    // Go to the dashboard page
+    // Render the EJS template to HTML with all required data passed as local variables
+    ejs.renderFile('views/admin/sales-report.ejs', { totalUsers, blockUser, mergedData,totalOrders, cancelledOrders, totalProduct, totalRevenue, deliveredOrders, mostSellingProduct, returnOrderCount, onlinePayment, pendingOrdersCount }, function(err, html){
+      if (err) {
+        console.error('Error generating HTML:', err);
+        res.status(500).send('An error occurred while generating the HTML');
+      } else {
+        var options = { format: 'Letter' };
+
+        // Create a PDF from the HTML string
+        pdf.create(html, options).toStream(function(err, stream){
+          if (err) {
+            console.error('Error generating PDF:', err);
+            res.status(500).send('An error occurred while generating the PDF');
+          } else {
+            // Set the filename for download
+            res.setHeader('Content-Disposition', 'attachment; filename="sales-report.pdf"');
+            stream.pipe(res);
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred');
+  }
+};
+
+exports.downloadSalesReportsExcel = async (req, res) => {
+  try {
+    // Get other necessary data
+    const { totalUsers, blockUser, mergedData, totalOrders, cancelledOrders, totalProduct, totalRevenue, deliveredOrders, mostSellingProduct, returnOrderCount, onlinePayment, pendingOrdersCount } = await getUserDetailsAndOrders();
+
+    // Create a new workbook
+    const workbook = new ExcelJS.Workbook();
+
+    // Add a new worksheet to the workbook
+    const worksheet = workbook.addWorksheet('Sales Report');
+
+    // Add data to the worksheet
+    worksheet.columns = [
+      { header: 'Total Users', key: 'totalUsers' },
+      { header: 'Blocked Users', key: 'blockUser' },
+      { header: 'Total Orders', key: 'totalOrders' },
+      { header: 'CancelledOrders ', key: 'cancelledOrders' },
+      { header: 'TotalProduct ', key: 'totalProduct' },
+      { header: 'TotalRevenue ', key: 'totalRevenue' },
+      { header: 'DeliveredOrders ', key: 'deliveredOrders' },
+      { header: 'onlinePayment ', key: 'onlinePayment' },
+      { header: 'returnOrderCount ', key: 'returnOrderCount' },
+
+      
+    ];
+
+    worksheet.addRow({ totalUsers, blockUser,onlinePayment,totalOrders,totalProduct ,totalRevenue,deliveredOrders,returnOrderCount});
+
+    // Write the Excel file to a stream
+    const stream = new require('stream').PassThrough();
+    await workbook.xlsx.write(stream);
+
+    // Set the headers for the download
+    res.setHeader('Content-Disposition', 'attachment; filename="sales-report.xlsx"');
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    // Send the Excel file to the client
+    stream.pipe(res);
+  } catch (error) {
+    console.error('Error generating Excel file:', error);
+    res.status(500).send('An error occurred while generating the Excel file');
+  }
+};
+
+
+
+
+
+
+
+// async function generatePDF() {
+//     // Launch a new browser instance
+//     const browser = await puppeteer.launch();
+
+//     // Create a new page
+//     const page = await browser.newPage();
+
+//     // Go to the dashboard page
    
-    await page.goto('http://localhost:3000/dashboard', { waitUntil: 'networkidle0' });
+//     await page.goto('http://localhost:3000/dashboard', { waitUntil: 'networkidle0' });
 
-    // Generate the PDF from the page content
-    const pdf = await page.pdf({ format: 'A4' });
+//     // Generate the PDF from the page content
+//     const pdf = await page.pdf({ format: 'A4' });
 
-    // Close the browser
-    await browser.close();
+//     // Close the browser
+//     await browser.close();
 
-    return pdf;
-}
+//     return pdf;
+// }
 
 
 const getTotalRevenue = async () => {
@@ -168,10 +161,10 @@ exports.getDashboardData = async (req, res) => {
 
         const totalRevenue = await getTotalRevenue();
 
-        
+        const blockUser = await User.countDocuments({ isBlocked: true });
       
       
-        res.render('adminhome', { totalUsers, totalOrders, cancelledOrders,totalproduct,totalRevenue});
+        res.render('adminhome', { totalUsers, totalOrders, cancelledOrders,blockUser,totalproduct,totalRevenue});
     } catch (error) {
         console.error('Error fetching dashboard data:', error.message);
         res.status(500).send('Internal Server Error');
@@ -262,24 +255,24 @@ exports.salesReport= async (req, res) => {
 
 
 
-async function generatePDF() {
-    // Launch a new browser instance
-    const browser = await puppeteer.launch();
+// async function generatePDF() {
+//     // Launch a new browser instance
+//     const browser = await puppeteer.launch();
 
-    // Create a new page
-    const page = await browser.newPage();
+//     // Create a new page
+//     const page = await browser.newPage();
 
     
-    await page.goto('http://localhost:3000/admin/getUserDetailsAndOrders', { waitUntil: 'networkidle0' });
+//     await page.goto('http://localhost:3000/admin/getUserDetailsAndOrders', { waitUntil: 'networkidle0' });
 
-    // Generate the PDF from the page content
-    const pdf = await page.pdf({ format: 'A4' });
+//     // Generate the PDF from the page content
+//     const pdf = await page.pdf({ format: 'A4' });
 
-    // Close the browser
-    await browser.close();
+//     // Close the browser
+//     await browser.close();
 
-    return pdf;
-}
+//     return pdf;
+// }
 
 
 exports.downloadSalesReport=async (req, res) => {
